dfbc6626e628a5b16ee365759faf412290f6fd02
alexgpitts <46640378+alexgpitts@users.noreply.github.com>
2019-02-26 21:55:56 -0800

    deleted some code

--- Assignment9/part2/main.js
+++ Assignment9/part2/main.js
@@ -1,36 +1,28 @@
+var player;
+function onYouTubeIframeAPIReady() {
+player2 = new YT.Player('player2', {
+    height: '390',
+    width: '640',
+    videoId: 'w-IEfjDTi9c',
+    events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+    }
+    });
+}
 
-      var tag = document.createElement('script');
 
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function onPlayerReady(event) {
+event.target.playVideo();
+}
 
 
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player2 = new YT.Player('player2', {
-            height: '390',
-            width: '640',
-            videoId: 'w-IEfjDTi9c',
-            events: {
-              'onReady': onPlayerReady,
-              'onStateChange': onPlayerStateChange
-            }
-          });
-      }
-
-     
-        function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-    
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
 
 
