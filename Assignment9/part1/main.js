2ba67212c7e65ba11a8b15203b712adf25cee483
alexgpitts <46640378+alexgpitts@users.noreply.github.com>
2019-02-28 10:27:49 -0800

    updates

--- Assignment9/part1/main.js
+++ Assignment9/part1/main.js
@@ -7,15 +7,17 @@ var People = function (name, age, occupation){
 }
 
 People.prototype.info = function() {
-    let info = ("name: " + this.name + " Age: " + this.age + " occupation: " + this.occupation);
+    let info = ("Name: " + this.name + ", Age: " + this.age + ", Occupation: " + this.occupation);
     return info;
 }
 
 var alex = new People('Alex', 20, 'landscaping');
+var leanna = new People('leanna', 47, 'facility services manager');
+var cody = new People('Cody', 25, 'USAF Hospital administrator');
 
 
 
-function print(){
+function printAlex(){
     var p = document.createElement('p');
     var Text = document.createTextNode(alex.info());
     p.appendChild(Text);
@@ -23,6 +25,26 @@ function print(){
     pageBody[0].appendChild(p);
 
 }
+
+
+function printLeanna(){
+    var p = document.createElement('p');
+    var Text = document.createTextNode(leanna.info());
+    p.appendChild(Text);
+    const pageBody = document.getElementsByClassName("people");
+    pageBody[0].appendChild(p);
+
+}
+
+function printCody(){
+    var p = document.createElement('p');
+    var Text = document.createTextNode(cody.info());
+    p.appendChild(Text);
+    const pageBody = document.getElementsByClassName("people");
+    pageBody[0].appendChild(p);
+
+}
+
 
 
 
