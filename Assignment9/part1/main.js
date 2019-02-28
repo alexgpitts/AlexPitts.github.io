6d0a937ecc3a43e45313157e80011ad1089c9064
alexgpitts <46640378+alexgpitts@users.noreply.github.com>
2019-02-28 10:17:52 -0800

    Update main.js

--- Assignment9/part1/main.js
+++ Assignment9/part1/main.js
@@ -1,23 +1,23 @@
-console.log()
-var people = function (name, age, occupation){
+
+var People = function (name, age, occupation){
     this.name = name;
     this.age = age;
     this.occupation = occupation;
    
 }
 
-people.prototype.info = function() {
-    console.log("name: " + this.name + " Age: " + this.age + " occupation: " + this.occupation);
+People.prototype.info = function() {
+    let info = ("name: " + this.name + " Age: " + this.age + " occupation: " + this.occupation);
+    return info;
 }
 
 var alex = new People('Alex', 20, 'landscaping');
 
-alex.info();
 
 
 function print(){
     var p = document.createElement('p');
-    var Text = document.createTextNode(alex.info);
+    var Text = document.createTextNode(alex.info());
     p.appendChild(Text);
     const pageBody = document.getElementsByClassName("people");
     pageBody[0].appendChild(p);
